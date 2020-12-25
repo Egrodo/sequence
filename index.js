446a643b51d27c048487a613fcbc5906bd42f5b3
Noah Yamamoto <egrodo1@gmail.com>
2020-12-25 15:00:58 -0500

    Made animation speed depend on time and added sound

--- index.js
+++ index.js
@@ -1,57 +1,70 @@
 // init
 (() => {
-  document.getElementById("startBtn").addEventListener("click", startSequence);
+  document.getElementById('startBtn').addEventListener('click', startSequence);
 
   // Register service worker
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
       navigator.serviceWorker
-        .register("service-worker.js")
+        .register('service-worker.js')
         .then(
-          () => console.log("Service worker registered!"),
-          () => console.log("Service worker registration failed: ", err)
+          () => console.log('Service worker registered!'),
+          () => console.log('Service worker registration failed: ', err)
         )
-        .catch(err => console.log(err));
+        .catch((err) => console.log(err));
     });
   } else {
-    console.log("service worker is not supported");
+    console.log('service worker is not supported');
   }
 })();
 
 // Helpers
-const getRealPos = (e, canvas) => [e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop];
+const getRealPos = (e, canvas) => [
+  e.pageX - canvas.offsetLeft,
+  e.pageY - canvas.offsetTop,
+];
 const colorGenerator = () => {
   const colors = [
-    ["#44AF69", "Green"],
-    ["#F8333C", "Red"],
-    ["#E59500", "Orange"],
-    ["#2B9EB3", "Cyan"],
-    ["#D8D52B", "Yellow"]
+    ['#44AF69', 'Green'],
+    ['#F8333C', 'Red'],
+    ['#E59500', 'Orange'],
+    ['#2B9EB3', 'Cyan'],
+    ['#D8D52B', 'Yellow'],
   ];
   let i = 0;
   return () => colors[i++ % colors.length];
 };
-const clearRect = ctx => ctx.clearRect(0, 0, canvas.width, canvas.height);
+const clearRect = (ctx) => ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 // Main application
 function startSequence() {
   // First remove welcome screen & display instructions
-  document.getElementById("welcomeScreen").style.opacity = "0";
-  document.getElementById("topTextBox").style.display = "block";
+  document.getElementById('welcomeScreen').style.opacity = '0';
+  document.getElementById('topTextBox').style.display = 'block';
 
   // Fade in
-  window.setTimeout(() => (document.getElementById("welcomeScreen").style.display = "none"), 500);
-  window.setTimeout(() => (document.getElementById("topTextBox").style.opacity = "1"), 700);
+  window.setTimeout(
+    () => (document.getElementById('welcomeScreen').style.display = 'none'),
+    500
+  );
+  window.setTimeout(
+    () => (document.getElementById('topTextBox').style.opacity = '1'),
+    700
+  );
 
   // Init canvas
-  const canvas = document.getElementById("canvas");
-  const context = canvas.getContext("2d");
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+
+  // Init audio
+  const audio = document.getElementsByTagName('audio')[0];
+  audio.playbackRate = 0.8;
 
   // Adjust size of canvas
   const { height, width } = canvas.getBoundingClientRect();
   canvas.height = height;
   canvas.width = width;
-  window.addEventListener("resize", () => {
+  window.addEventListener('resize', () => {
     const { height, width } = canvas.getBoundingClientRect();
     canvas.height = height;
     canvas.width = width;
@@ -69,14 +82,14 @@ function startSequence() {
       return;
     }
 
-    if (document.getElementById("topTextBox").style.opacity === "1") {
-      document.getElementById("topTextBox").style.opacity = 0;
-      document.getElementById("topTextBox").style.display = "none";
+    if (document.getElementById('topTextBox').style.opacity === '1') {
+      document.getElementById('topTextBox').style.opacity = 0;
+      document.getElementById('topTextBox').style.display = 'none';
     }
 
-    if (document.getElementById("centeredTextBox").innerText) {
+    if (document.getElementById('centeredTextBox').innerText) {
       clearRect(context);
-      document.getElementById("centeredTextBox").innerText = "";
+      document.getElementById('centeredTextBox').innerText = '';
     }
 
     for (let i = 0; i < e.touches.length; ++i) {
@@ -141,13 +154,17 @@ function startSequence() {
 
     if (touches.size < 2 && playingAnimation && !playingWinAnimation) {
       playingAnimation = false;
+      audio.pause();
     }
     if (touches.size === 0 && !playingAnimation && !playingWinAnimation) {
       // If the user has not touched the screen for more than 5 seconds, add the instructions back.
       window.setTimeout(() => {
         if (!playingAnimation && !playingWinAnimation) {
-          document.getElementById("topTextBox").style.display = "block";
-          window.setTimeout(() => (document.getElementById("topTextBox").style.opacity = "1"), 700);
+          document.getElementById('topTextBox').style.display = 'block';
+          window.setTimeout(
+            () => (document.getElementById('topTextBox').style.opacity = '1'),
+            700
+          );
         }
       }, 5000);
     }
@@ -157,21 +174,28 @@ function startSequence() {
   function quickTouch(e) {
     if (!playingAnimation && !playingWinAnimation) {
       clearRect(context);
-      document.getElementById("centeredTextBox").innerText = "";
+      document.getElementById('centeredTextBox').innerText = '';
     }
   }
 
-  canvas.addEventListener("touchstart", touchStart);
-  canvas.addEventListener("touchmove", touchMove);
-  canvas.addEventListener("touchend", touchEnd);
-  canvas.addEventListener("click", quickTouch);
+  canvas.addEventListener('touchstart', touchStart);
+  canvas.addEventListener('touchmove', touchMove);
+  canvas.addEventListener('touchend', touchEnd);
+  canvas.addEventListener('click', quickTouch);
 
   function startAnimation() {
     // Animate the circles
 
-    let arcSize = 100;
     const arrayOfTouches = Array.from(touches);
-    const winner = arrayOfTouches[Math.floor(Math.random() * arrayOfTouches.length)];
+    const winner =
+      arrayOfTouches[Math.floor(Math.random() * arrayOfTouches.length)];
+
+    // Calculate what percentage of the maxArcSize to decrement by based on time passed since start and animationTime
+    let arcSize = 100; // This will decrement to zero over animationTime seconds.
+    const animationTime = 3 * 1000; // 3 seconds
+    const startTime = Date.now();
+    audio.currentTime = 0;
+    audio.play();
     const animateTouches = () => {
       if (playingAnimation) {
         touches.forEach((touch, identifier) => {
@@ -179,7 +203,7 @@ function startSequence() {
           // Draw outer circle
           context.beginPath();
           context.arc(x, y, arcSize * 10, 0, 2 * Math.PI);
-          context.fillStyle = "black";
+          context.fillStyle = 'black';
           context.fill();
 
           // Draw inner circle
@@ -193,11 +217,18 @@ function startSequence() {
           context.fill();
         });
 
-        arcSize -= 0.5;
-        if (arcSize < 1 && !playingWinAnimation) {
+        const currTime = Date.now();
+        // What percentage from startTime to startTime + animationTime is currTime?
+        // ((input - min) * 100) / (max - min)
+        const percentComplete =
+          ((currTime - startTime) * 100) /
+          (startTime + animationTime - startTime);
+        arcSize = 100 - percentComplete;
+        if (arcSize <= 1 && !playingWinAnimation) {
           playingAnimation = false;
           startWinAnimation(winner[1]);
         } else {
+          console.log(arcSize);
           window.requestAnimationFrame(animateTouches);
         }
       } else clearRect(context);
@@ -212,6 +243,8 @@ function startSequence() {
     // Short vibration
     window.navigator.vibrate(200);
 
+    audio.pause();
+
     let arcSize = 0;
     const winAnimation = () => {
       if (playingWinAnimation) {
@@ -223,7 +256,7 @@ function startSequence() {
         // Draw a black circle where the winners finger was for increased clarity
         context.beginPath();
         context.arc(x, y, 20, 0, 2 * Math.PI);
-        context.strokeStyle = "black";
+        context.strokeStyle = 'black';
         context.lineWidth = 5;
         context.stroke();
 
@@ -234,7 +267,9 @@ function startSequence() {
 
         window.requestAnimationFrame(winAnimation);
       } else {
-        document.getElementById("centeredTextBox").innerText = `${color[1]} wins!`;
+        document.getElementById(
+          'centeredTextBox'
+        ).innerText = `${color[1]} wins!`;
       }
     };
 
@@ -244,7 +279,7 @@ function startSequence() {
   let visibilityTimerRef = null;
   // Use the interaction observer API to detect when the user has been off the app for some time and refresh the welcome screen
   const visibilityChange = () => {
-    console.log("visible change");
+    console.log('visible change');
     if (document.hidden) {
       if (visibilityTimerRef) {
         window.clearTimeout(visibilityTimerRef);
@@ -253,12 +288,12 @@ function startSequence() {
         // If the page not visible for longer than 5 seconds, fade back in the welcome screen.
         visibilityTimerRef = window.setTimeout(() => {
           clearRect(context);
-          document.getElementById("welcomeScreen").style.display = "block";
-          document.getElementById("welcomeScreen").style.opacity = 1;
+          document.getElementById('welcomeScreen').style.display = 'block';
+          document.getElementById('welcomeScreen').style.opacity = 1;
         }, 5000);
       }
     }
   };
 
-  document.addEventListener("visibilitychange", visibilityChange);
+  document.addEventListener('visibilitychange', visibilityChange);
 }
